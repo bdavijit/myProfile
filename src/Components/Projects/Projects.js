d54e3399fdd812fc717cf48b485f0042ccda7d9c
bdavijit <bdavijit@gmail.com>
2022-08-25 12:55:06 +0600

    add projects

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,7 +1,71 @@
 import React from 'react';
+import "./Projects.css";
 
 const Projects = () => {
-    return <div>Projects</div>;
+    return (
+          <>
+                <h1 className='text-center'>Projects</h1>
+                <section className='ProjectsBox'>
+                      <section className='ProjectFrame'>
+                            <iframe
+                                  src='https://router-firebase-integrat-3357a.web.app/'
+                                  title='W3Schools Free Online Web Tutorials'
+                                  className='frameSize'
+                            ></iframe>
+                            <div>
+                                  <h1 className='text-center'>Grocery HUB</h1>
+                                  <p>
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit. Id impedit
+                                        consequuntur architecto, adipisci
+                                        quibusdam fuga rem dolore blanditiis
+                                        soluta velit quis quisquam atque?
+                                        Delectus necessitatibus voluptatum nemo
+                                        maxime, ducimus molestiae?
+                                  </p>
+                            </div>
+                      </section>
+                      <section className='ProjectFrame'>
+                            <iframe
+                                  src='https://biostar2-d38fa.web.app/'
+                                  title='W3Schools Free Online Web Tutorials'
+                                  className='frameSize'
+                            ></iframe>
+                            <div>
+                                  <h1 className='text-center'>Biostar</h1>
+                                  <p>
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit. Id impedit
+                                        consequuntur architecto, adipisci
+                                        quibusdam fuga rem dolore blanditiis
+                                        soluta velit quis quisquam atque?
+                                        Delectus necessitatibus voluptatum nemo
+                                        maxime, ducimus molestiae?
+                                  </p>
+                            </div>
+                      </section>
+                      <section className='ProjectFrame'>
+                            <iframe
+                                  src='https://biostar2-d38fa.web.app/'
+                                  title='W3Schools Free Online Web Tutorials'
+                                  className='frameSize'
+                            ></iframe>
+                            <div>
+                                  <h1 className='text-center'>Biostar</h1>
+                                  <p>
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit. Id impedit
+                                        consequuntur architecto, adipisci
+                                        quibusdam fuga rem dolore blanditiis
+                                        soluta velit quis quisquam atque?
+                                        Delectus necessitatibus voluptatum nemo
+                                        maxime, ducimus molestiae?
+                                  </p>
+                            </div>
+                      </section>
+                </section>
+          </>
+    );
 };
 
 export default Projects;
