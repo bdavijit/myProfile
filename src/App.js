51c98f25d0c9da3129390c038ad5c0ec4aa5b187
bdavijit <bdavijit@gmail.com>
2022-08-25 12:15:48 +0600

    design

--- src/App.js
+++ src/App.js
@@ -8,26 +8,36 @@ import Navbar from './Components/Navbar/Navbar';
 import Notfound from './Components/Notfound/Notfound';
 import Projects from './Components/Projects/Projects';
 import Skills from './Components/Skills/Skills';
-import './style.js/style.css';
+import TopNavbar from './Components/TopNavbar/TopNavbar';
+import './style/style.css';
 
 function App() {
   return (
-        <div className='MainBox'>
-              <div className='MainSubBox menuBox'>
-                    <Navbar />
-              </div>
-              <div>
-                    <Routes>
-                          <Route path='/' element={<Home />} />
-                          <Route path='/Skills' element={<Skills />} />
-                          <Route path='/Projects' element={<Projects />} />
-                          <Route path='/Education' element={<Education />} />
-                          <Route path='/Contact' element={<Contact />} />
+        <>
+         <TopNavbar />
+              <div className='MainBox'>
+                    <div className='MainSubBox menuBox'>
+                          <Navbar />
+                    </div>
+                    <div>
+                          <Routes>
+                                <Route path='/' element={<Home />} />
+                                <Route path='/Skills' element={<Skills />} />
+                                <Route
+                                      path='/Projects'
+                                      element={<Projects />}
+                                />
+                                <Route
+                                      path='/Education'
+                                      element={<Education />}
+                                />
+                                <Route path='/Contact' element={<Contact />} />
 
-                          <Route path='*' element={<Notfound />} />
-                    </Routes>
+                                <Route path='*' element={<Notfound />} />
+                          </Routes>
+                    </div>
               </div>
-        </div>
+        </>
   );
 }
 
---
+++ src/Components/TopNavbar/TopNavbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./TopNavbar.css"
+
+const TopNavbar = () => {
+    return (
+          <div className='TopNavbar'>
+                TopNavbar
+                {/* <i
+                      class='fas fa-chevron-down'
+                      style='font-size:48px;color:red'
+                ></i> */}
+          </div>
+    );
+};
+
+export default TopNavbar;
