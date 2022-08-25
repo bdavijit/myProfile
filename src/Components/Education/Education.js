e7d703382a59c321edafdda022f52cf00218484f
bdavijit <bdavijit@gmail.com>
2022-08-25 12:19:01 +0600

    add data

--- src/Components/Education/Education.js
+++ src/Components/Education/Education.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const Education = () => {
-    return <div>Education</div>;
+    return (
+          <>
+                
+                      <h1>Education Qualification</h1>
+                      <section>
+                            <h4>Bsc in CSE</h4>
+                            <h3>Daffodil International University</h3>
+                            <h5>3.49 out of 4 </h5>
+                            <br></br>
+                            <h3>HSC in Computer Science</h3>
+                            <h2>N.s Govt College</h2>
+                            <h5>4.7 out of 5 </h5>
+                            <br></br>
+                            <h3>SSC in Science</h3>
+                            <h2>Natore Govt High School</h2>
+                            <h5>4.69 out of 5 </h5>
+                      </section>
+              
+          </>
+    );
 };
 
 export default Education;
